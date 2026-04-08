4138bc79f0c701695039f29b771a007efafd1a14
Mouhib Yassine <87534925+none159@users.noreply.github.com>
2026-04-08 22:42:39 +0200

    Remove: missing-number - submission-6

--- Data Structures & Algorithms/missing-number/submission-6.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        l = []
-        for i in range(len(nums) + 1):
-            if i not in nums:
-                return i
-     
-        return 0
