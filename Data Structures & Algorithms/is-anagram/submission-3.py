1001993a51b124a4ee42132a66863cd4101ff630
Yassine Mouhib <noneenone019@gmail.com>
2026-04-12 23:55:14 +0100

    Remove: is-anagram - submission-3

--- Data Structures & Algorithms/is-anagram/submission-3.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        s = list(s)
-        t = list(t)
-        s.sort()
-        t.sort()
-        if s == t:
-            return True
-        return False
