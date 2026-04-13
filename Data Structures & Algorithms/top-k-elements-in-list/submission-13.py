3e761b4841fcfef83fbcf155b604173978e88ce9
Yassine Mouhib <noneenone019@gmail.com>
2026-04-13 01:14:13 +0100

    Add: top-k-elements-in-list - submission-13

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-13.py
@@ -0,0 +1,10 @@
+from heapq import nlargest
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = {}
+        for i in range(len(nums)):
+            if nums[i] not in freq:
+                freq[nums[i]] = 1
+            else:
+                freq[nums[i]] += 1
+        return nlargest(k,freq,key=freq.get)
